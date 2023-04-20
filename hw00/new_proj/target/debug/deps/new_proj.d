875c0fa923907161f0f4acf419681d96f81dff34
lkb <tmwlak@gmail.com>
2023-04-20 14:25:12 +0900

    _

---
+++ hw00/new_proj/target/debug/deps/new_proj.d
@@ -0,0 +1,5 @@
+J:\project\study\rust-cis198-homework\hw00\new_proj\target\debug\deps\new_proj.exe: src\main.rs
+
+J:\project\study\rust-cis198-homework\hw00\new_proj\target\debug\deps\new_proj.d: src\main.rs
+
+src\main.rs:
---
+++ hw00/new_proj/target/debug/new_proj.d
@@ -0,0 +1,1 @@
+J:\project\study\rust-cis198-homework\hw00\new_proj\target\debug\new_proj.exe: J:\project\study\rust-cis198-homework\hw00\new_proj\src\main.rs
